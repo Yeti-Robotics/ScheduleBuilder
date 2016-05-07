e42a8042fb2ad154f2ac52fdd19091430701fb2d
wazateer1 <wazateer1@gmail.com>
2016-05-07 11:43:59 -0400

    - Finished Tab system

--- js/ScheduleBuilder.js
+++ js/ScheduleBuilder.js
@@ -49,17 +49,49 @@ app.controller("ScheduleBuilderController", function ($rootScope, $scope) {
 	$scope.bringForward = function (panel) {
 		switch (panel) {
 		case 'people':
+			$(".panel").addClass("hidden");
+			$(".tab").removeClass("active");
+			$("#people").removeClass("hidden");
+			$("#people-tab").addClass("active");
+			$("#skills").removeClass("hidden");
+			$("#skills-tab").addClass("active");
 			break;
 		case 'skills':
+			$(".panel").addClass("hidden");
+			$(".tab").removeClass("active");
+			$("#skills").removeClass("hidden");
+			$("#skills-tab").addClass("active");
 			break;
 		case 'multi-skill-roles':
+			$(".panel").addClass("hidden");
+			$(".tab").removeClass("active");
+			$("#multi-skill-roles").removeClass("hidden");
+			$("#multi-skill-roles-tab").addClass("active");
+			$("#skills").removeClass("hidden");
+			$("#skills-tab").addClass("active");
 			break;
 		case 'team-archetypes':
+			$(".panel").addClass("hidden");
+			$(".tab").removeClass("active");
+			$("#team-archetypes").removeClass("hidden");
+			$("#team-archetypes-tab").addClass("active");
+			$("#skills").removeClass("hidden");
+			$("#skills-tab").addClass("active");
 			break;
 		case 'teams':
+			$(".panel").addClass("hidden");
+			$(".tab").removeClass("active");
+			$("#teams").removeClass("hidden");
+			$("#teams-tab").addClass("active");
+			$("#people").removeClass("hidden");
+			$("#people-tab").addClass("active");
 			break;
 		case 'schedule':
+			$(".panel").addClass("hidden");
+			$(".tab").removeClass("active");
+			$("#schedule").removeClass("hidden");
+			$("#schedule-tab").addClass("active");
 			break;
 		}
-	}
+	};
 });
