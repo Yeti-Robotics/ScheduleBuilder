766980bd8f629e3fd89da9841898706b29b5386a
Sam Perlmutter <sam.perlmutter227@gmail.com>
2016-05-10 16:22:58 -0400

    Revert "- Hopefully fixed .gitignore"
    This reverts commit 47cead59b190ce24c92e2b91dff7d14c5e60ab16.

--- php/insertSchedule.php
+++ php/insertSchedule.php
@@ -4,25 +4,16 @@ include("connect.php");
 
 $schedule = json_decode(file_get_contents("php://input"), true)["schedule"];
 
-if (isset($schedule["schedule"]) &&
-   isset($schedule["people"]) &&)
-   isset($schedule["skills"]) &&)
-   isset($schedule["multiSkillRoles"]) &&)
-   isset($schedule["teamArchetypes"]) &&)
-   isset($schedule["teams"]) &&)
-   isset($schedule["days"])) {
-	$document = array(
-		"schedule" => $schedule["schedule"],
-		"people" => $schedule["people"],
-		"skills" => $schedule["skills"],
-		"multiSkillRoles" => $schedule["multiSkillRoles"],
-		"teamArchetypes" => $schedule["teamArchetypes"],
-		"teams" => $schedule["teams"],
-		"days" => $schedule["days"]
-	);
-	$collection->insert($document);
-} else {
-	die(json_encode(array("error" => "Try submitting again with all the fields")));
-}
+$document = array(
+	"schedule" => $schedule["schedule"],
+	"people" => $schedule["people"],
+	"skills" => $schedule["skills"],
+	"multiSkillRoles" => $schedule["multiSkillRoles"],
+	"teamArchetypes" => $schedule["teamArchetypes"],
+	"teams" => $schedule["teams"],
+	"days" => $schedule["days"]
+);
+
+$collection->insert($document);
 
 ?>
